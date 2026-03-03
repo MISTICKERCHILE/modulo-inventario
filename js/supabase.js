9037e5614058097d5741504da6ae4977b4a29436
MISTICKERCHILE <contacto@misticker.cl>
2026-03-03 11:36:25 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,3 +1,3 @@
 const supabaseUrl = 'https://gdtrkrnxkhiqimvmfrms.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkdHJrcm54a2hpcWltdm1mcm1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NjYyODcsImV4cCI6MjA4ODA0MjI4N30.IYJR0fsehO0nXZ5YdEQ53S7Q1Z0e8qYSWdDeQkcTDS0'; // Asegúrate de que sea tu llave real
-const clienteSupabase = supabase.createClient(supabaseUrl, supabaseKey);
+window.clienteSupabase = supabase.createClient(supabaseUrl, supabaseKey);
